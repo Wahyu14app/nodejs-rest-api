a257dafb7bdc060fff409e73e278fc4abcd79611
Wahyu14app <kioskas.official@gmail.com>
2024-05-29 18:03:48 +0700

    v1

--- src/app.js
+++ src/app.js
@@ -13,4 +13,11 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 
+
+// Middleware untuk menangani rute yang tidak ditemukan (404)
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Not Found' });
+});
+
+
 module.exports = app;
